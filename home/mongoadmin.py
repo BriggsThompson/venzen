a576c67c4ab17ec033487a3f775be3ca9a0845de
Briggs Thompson <briggs@rmn.com>
2015-03-09 18:13:59 -0500

    more admin stuff

---
+++ home/mongoadmin.py
@@ -0,0 +1,17 @@
+from home.models import Venue#, Space, Cost
+
+__author__ = 'briggs'
+
+# Import the MongoAdmin base class
+from mongonaut.sites import MongoAdmin
+
+# Import your custom models
+
+class VenueAdmin(MongoAdmin):
+
+    search_fields = ('name')
+    readonly_fields = ('venueId')
+    #prepopulated_fields = { 'spaces_costs_daysofweek' : ('mon', 'tue', 'wed',)}
+    ordering = ('name', 'description', 'mission')
+
+Venue.mongoadmin = VenueAdmin()
