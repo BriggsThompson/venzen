8a000cab5dedcd44e7fd870b689451e26600f245
Briggs Thompson <briggs@rmn.com>
2015-03-31 11:41:12 -0500

    Adding readme and some instructions

--- schema/alterations.sql
+++ schema/alterations.sql
@@ -1,6 +1,6 @@
--- alter table space modify deposit decimal null;
--- alter table space modify cleaningFee decimal null;
+alter table space modify deposit decimal null;
+alter table space modify cleaningFee decimal null;
 
--- alter table venue add column totalCapacity mediumint null after email;
--- alter table detail modify title varchar(1000) not null;
--- alter table detail modify description text;
+alter table venue add column totalCapacity mediumint null after email;
+alter table detail modify title varchar(1000) not null;
+alter table detail modify description text;
